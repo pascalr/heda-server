b4adc8f6a1599a4d4dd154b978b5943a8faba6a2
Dark Krael <dark.krael@gmail.com>
2022-09-19 13:59:45 -0400

    .

---
+++ router.js
@@ -0,0 +1,265 @@
+var express = require('express');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var crypto = require('crypto');
+var db = require('./db');
+var ensureLogIn = require('connect-ensure-login').ensureLoggedIn;
+
+var ensureLoggedIn = ensureLogIn();
+var router = express.Router();
+
+/* Configure password authentication strategy.
+ *
+ * The `LocalStrategy` authenticates users by verifying a username and password.
+ * The strategy parses the username and password from the request and calls the
+ * `verify` function.
+ *
+ * The `verify` function queries the database for the user record and verifies
+ * the password by hashing the password supplied by the user and comparing it to
+ * the hashed password stored in the database.  If the comparison succeeds, the
+ * user is authenticated; otherwise, not.
+ */
+passport.use(new LocalStrategy(function verify(username, password, cb) {
+  db.get('SELECT * FROM users WHERE username = ?', [ username ], function(err, row) {
+    if (err) { return cb(err); }
+    if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+    
+    crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+      if (err) { return cb(err); }
+      if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+        return cb(null, false, { message: 'Incorrect username or password.' });
+      }
+      return cb(null, row);
+    });
+  });
+}));
+
+/* Configure session management.
+ *
+ * When a login session is established, information about the user will be
+ * stored in the session.  This information is supplied by the `serializeUser`
+ * function, which is yielding the user ID and username.
+ *
+ * As the user interacts with the app, subsequent requests will be authenticated
+ * by verifying the session.  The same user information that was serialized at
+ * session establishment will be restored when the session is authenticated by
+ * the `deserializeUser` function.
+ *
+ * Since every request to the app needs the user ID and username, in order to
+ * fetch todo records and render the user element in the navigation bar, that
+ * information is stored in the session.
+ */
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    cb(null, { id: user.id, username: user.username });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+/* GET /login
+ *
+ * This route prompts the user to log in.
+ *
+ * The 'login' view renders an HTML form, into which the user enters their
+ * username and password.  When the user submits the form, a request will be
+ * sent to the `POST /login/password` route.
+ */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/* POST /login/password
+ *
+ * This route authenticates the user by verifying a username and password.
+ *
+ * A username and password are submitted to this route via an HTML form, which
+ * was rendered by the `GET /login` route.  The username and password is
+ * authenticated using the `local` strategy.  The strategy will parse the
+ * username and password from the request and call the `verify` function.
+ *
+ * Upon successful authentication, a login session will be established.  As the
+ * user interacts with the app, by clicking links and submitting forms, the
+ * subsequent requests will be authenticated by verifying the session.
+ *
+ * When authentication fails, the user will be re-prompted to login and shown
+ * a message informing them of what went wrong.
+ */
+router.post('/login/password', passport.authenticate('local', {
+  successReturnToOrRedirect: '/',
+  failureRedirect: '/login',
+  failureMessage: true
+}));
+
+/* POST /logout
+ *
+ * This route logs the user out.
+ */
+router.post('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+/* GET /signup
+ *
+ * This route prompts the user to sign up.
+ *
+ * The 'signup' view renders an HTML form, into which the user enters their
+ * desired username and password.  When the user submits the form, a request
+ * will be sent to the `POST /signup` route.
+ */
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
+/* POST /signup
+ *
+ * This route creates a new user account.
+ *
+ * A desired username and password are submitted to this route via an HTML form,
+ * which was rendered by the `GET /signup` route.  The password is hashed and
+ * then a new user record is inserted into the database.  If the record is
+ * successfully created, the user is logged in.
+ */
+router.post('/signup', function(req, res, next) {
+  var salt = crypto.randomBytes(16);
+  crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+    if (err) { return next(err); }
+    db.run('INSERT INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
+      req.body.username,
+      hashedPassword,
+      salt
+    ], function(err) {
+      if (err) { return next(err); }
+      var user = {
+        id: this.lastID,
+        username: req.body.username
+      };
+      req.login(user, function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+      });
+    });
+  });
+});
+
+function fetchTodos(req, res, next) {
+  db.all('SELECT * FROM todos WHERE owner_id = ?', [
+    req.user.id
+  ], function(err, rows) {
+    if (err) { return next(err); }
+    
+    var todos = rows.map(function(row) {
+      return {
+        id: row.id,
+        title: row.title,
+        completed: row.completed == 1 ? true : false,
+        url: '/' + row.id
+      }
+    });
+    res.locals.todos = todos;
+    res.locals.activeCount = todos.filter(function(todo) { return !todo.completed; }).length;
+    res.locals.completedCount = todos.length - res.locals.activeCount;
+    next();
+  });
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  if (!req.user) { return res.render('home'); }
+  next();
+}, fetchTodos, function(req, res, next) {
+  res.locals.filter = null;
+  res.render('index', { user: req.user });
+});
+
+router.get('/active', ensureLoggedIn, fetchTodos, function(req, res, next) {
+  res.locals.todos = res.locals.todos.filter(function(todo) { return !todo.completed; });
+  res.locals.filter = 'active';
+  res.render('index', { user: req.user });
+});
+
+router.get('/completed', ensureLoggedIn, fetchTodos, function(req, res, next) {
+  res.locals.todos = res.locals.todos.filter(function(todo) { return todo.completed; });
+  res.locals.filter = 'completed';
+  res.render('index', { user: req.user });
+});
+
+router.post('/', ensureLoggedIn, function(req, res, next) {
+  req.body.title = req.body.title.trim();
+  next();
+}, function(req, res, next) {
+  if (req.body.title !== '') { return next(); }
+  return res.redirect('/' + (req.body.filter || ''));
+}, function(req, res, next) {
+  db.run('INSERT INTO todos (owner_id, title, completed) VALUES (?, ?, ?)', [
+    req.user.id,
+    req.body.title,
+    req.body.completed == true ? 1 : null
+  ], function(err) {
+    if (err) { return next(err); }
+    return res.redirect('/' + (req.body.filter || ''));
+  });
+});
+
+router.post('/:id(\\d+)', ensureLoggedIn, function(req, res, next) {
+  req.body.title = req.body.title.trim();
+  next();
+}, function(req, res, next) {
+  if (req.body.title !== '') { return next(); }
+  db.run('DELETE FROM todos WHERE id = ? AND owner_id = ?', [
+    req.params.id,
+    req.user.id
+  ], function(err) {
+    if (err) { return next(err); }
+    return res.redirect('/' + (req.body.filter || ''));
+  });
+}, function(req, res, next) {
+  db.run('UPDATE todos SET title = ?, completed = ? WHERE id = ? AND owner_id = ?', [
+    req.body.title,
+    req.body.completed !== undefined ? 1 : null,
+    req.params.id,
+    req.user.id
+  ], function(err) {
+    if (err) { return next(err); }
+    return res.redirect('/' + (req.body.filter || ''));
+  });
+});
+
+router.post('/:id(\\d+)/delete', ensureLoggedIn, function(req, res, next) {
+  db.run('DELETE FROM todos WHERE id = ? AND owner_id = ?', [
+    req.params.id,
+    req.user.id
+  ], function(err) {
+    if (err) { return next(err); }
+    return res.redirect('/' + (req.body.filter || ''));
+  });
+});
+
+router.post('/toggle-all', ensureLoggedIn, function(req, res, next) {
+  db.run('UPDATE todos SET completed = ? WHERE owner_id = ?', [
+    req.body.completed !== undefined ? 1 : null,
+    req.user.id
+  ], function(err) {
+    if (err) { return next(err); }
+    return res.redirect('/' + (req.body.filter || ''));
+  });
+});
+
+router.post('/clear-completed', ensureLoggedIn, function(req, res, next) {
+  db.run('DELETE FROM todos WHERE owner_id = ? AND completed = ?', [
+    req.user.id,
+    1
+  ], function(err) {
+    if (err) { return next(err); }
+    return res.redirect('/' + (req.body.filter || ''));
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,52 +1,90 @@
-var connect = require('connect');
-var urlparser = require('url');
+#!/usr/bin/env node
 
-var authCheck = function (req, res, next) {
-  url = req.urlp = urlparser.parse(req.url, true);
+/**
+ * Module dependencies.
+ */
 
-  // ####
-  // Logout
-  if ( url.pathname == "/logout" ) {
-    req.session.destroy();
+var app = require('./app');
+var debug = require('debug')('todos:server');
+var http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
   }
 
-  // ####
-  // Is User already validated?
-  if (req.session && req.session.auth == true) {
-    next(); // stop here and pass to the next onion ring of connect
-    return;
+  if (port >= 0) {
+    // port number
+    return port;
   }
 
-  // ########
-  // Auth - Replace this example with your Database, Auth-File or other things
-  // If Database, you need a Async callback...
-  if ( url.pathname == "/login" && 
-       url.query.name == "max" && 
-       url.query.pwd == "herewego"  ) {
-    req.session.auth = true;
-    next();
-    return;
-  }
-
-  // ####
-  // This user is not authorized. Stop talking to him.
-  res.writeHead(403);
-  res.end('Sorry you are not authorized.\n\nFor a login use: /login?name=max&pwd=herewego');
-  return;
+  return false;
 }
 
-var helloWorldContent = function (req, res, next) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('authorized. Walk around :) or use /logout to leave\n\nYou are currently at '+req.urlp.pathname);
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 }
 
-var server = connect.createServer(
-  //connect.logger({ format: ':method :url' }),
-  connect.cookieParser(),
-  connect.session({ secret: 'foobar' }),
-  connect.bodyParser(),
-  authCheck,
-  helloWorldContent
-);
+/**
+ * Event listener for HTTP server "listening" event.
+ */
 
-server.listen(3000);
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
