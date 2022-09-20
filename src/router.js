ce67f48102015062bf62a8d7185021680c1e9ddb
Dark Krael <dark.krael@gmail.com>
2022-09-20 14:46:40 -0400

    .

--- src/router.js
+++ src/router.js
@@ -157,6 +157,95 @@ router.post('/signup', function(req, res, next) {
   });
 });
 
+// https://stackoverflow.com/questions/20277020/how-to-reset-change-password-in-node-js-with-passport-js
+router.get('/forgot', function (req, res, next) {
+  if (req.isAuthenticated()) { return res.redirect('/'); /* user is alreay logged in */ }
+  res.render('forgot');
+});
+
+// https://stackoverflow.com/questions/20277020/how-to-reset-change-password-in-node-js-with-passport-js
+router.post('/forgot', function (req, res, next) {
+  if (req.isAuthenticated()) { return res.redirect('/'); /* user is alreay logged in */ }
+  users.forgot(req, res, function (err) {
+    if (err) {
+      req.flash('error', err);
+    }
+    else {
+      req.flash('success', 'Please check your email for further instructions.');
+    }
+    res.redirect('/');
+  });
+});
+
+// https://stackoverflow.com/questions/20277020/how-to-reset-change-password-in-node-js-with-passport-js
+router.get('/reset/:token', function (req, res, next) {
+  if (req.isAuthenticated()) { return res.redirect('/'); /* user is alreay logged in */ }
+
+  var token = req.params.token;
+  res.locals.token = token;
+  //users.checkReset(token, req, res, function (err, data) {
+  //  if (err)
+  //    req.flash('error', err);
+
+  //  //show the UI with new password entry
+  //  res.render('reset');
+  //});
+  res.render('reset');
+});
+
+// https://stackoverflow.com/questions/20277020/how-to-reset-change-password-in-node-js-with-passport-js
+router.post('/reset', function (req, res, next) {
+  if (req.isAuthenticated() || !req.body.token) { return res.redirect('/'); /* user is alreay logged in */ }
+  
+  var salt = crypto.randomBytes(16);
+  crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+    if (err) { return next(err); }
+    db.run('UPDATE users SET encrypted_password = ?, salt = ?, updated_at = ? WHERE reset_password_token = ?', [
+        hashedPassword, salt, nowStr(), req.body.token
+      ], function(err) {
+        if (err) { return next(err); }
+        var user = {
+          id: this.lastID,
+          username: req.body.username
+        };
+        req.login(user, function(err) {
+          if (err) { return next(err); }
+          res.redirect('/');
+        });
+      }
+    );
+  });
+  //db.get('SELECT * FROM users WHERE token = ?', [ req.body.token ], function(err, row) {
+  //  if (err) { return cb(err); }
+  //  if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+  // 
+  //  console.log('password: ', password)
+  //  console.log('salt: ', row.salt)
+  //  crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+  //    console.log('hashed: ', hashedPassword)
+  //    console.log('encrypted: ', row.encrypted_password)
+  //    var buf = typeof row.encrypted_password === 'string' ? Buffer.from(row.encrypted_password, 'utf8') : row.encrypted_password
+  //    console.log('buf: ', buf)
+  //    console.log('heeeeeeeeeeeeeeeeerrrrrrrrrrrrrrrrrrrrrreeeeeeeeeeeeeeeeeeeeeeee')
+  //    if (err) { return cb(err); }
+  //    if (!crypto.timingSafeEqual(buf, hashedPassword)) {
+  //      return cb(null, false, { message: 'Incorrect username or password.' });
+  //    }
+  //    return cb(null, row);
+  //  });
+  //});
+  //users.reset(req, res, function (err) {
+  //  if (err) {
+  //    req.flash('error', err);
+  //    return res.redirect('/reset');
+  //  }
+  //  else {
+  //    req.flash('success', 'Password successfully reset.  Please login using new password.');
+  //    return res.redirect('/login');
+  //  }
+  //});
+});
+
 function fetchRecipes(req, res, next) {
   db.all('SELECT * FROM recipes WHERE user_id = ?', [
     req.user.id
