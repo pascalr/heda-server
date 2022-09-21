b53a21010c5707be92f63eaac45ac0710ad9a696
Dark Krael <dark.krael@gmail.com>
2022-09-21 10:12:14 -0400

    .

--- src/router.js
+++ src/router.js
@@ -128,43 +128,61 @@ router.post('/reset', function (req, res, next) {
   });
 });
 
-function fetchTable(tableName) {
-}
+//class AppController < ApplicationController
+//  def index
+//    gon.suggestions = current_user.suggestions.includes(:recipe).map {|s| o = s.to_obj_with_recipe_info}
+//    gon.current_user_admin = current_user_admin?
+//    gon.user_tags = current_user.user_tags.order(:position).map {|t| t.to_obj}
+//    gon.recipe_filters = RecipeFilter.where(user_id: nil).or(current_user.recipe_filters).map {|f| f.to_obj }
+//    gon.favorite_recipes = current_user.favorite_recipes.includes(:recipe).sort_by {|fav| fav.recipe.name}.map{|fav| fav.to_obj}
+//    gon.machines = current_user.machines.map {|m| m.to_obj}
+//    gon.containers = current_user.containers.map {|c| c.to_obj}
+//    gon.machine_foods = current_user.machine_foods.includes(:food).sort_by(&:name).map {|f| f.to_obj}
+//    gon.container_quantities = current_user.container_quantities.includes(:container_format).map {|c| c.to_obj}
+//    gon.mixes = current_user.mixes.map {|e| e.to_obj}
+//    gon.recipes = current_user.recipes.order(:name).map {|e| e.to_obj}
+//    gon.ingredient_sections = IngredientSection.where(recipe_id: gon.recipes.map{|e|e[:id]}).map {|e| e.to_obj}
+//    gon.recipe_kinds = RecipeKind.order(:name).map {|e| e.to_obj(only: [:name, :image_id])}
+//    gon.images = Image.where(id: gon.recipes.map{|e|e[:image_id]}+gon.recipe_kinds.map{|e|e[:image_id]}).map {|e| e.to_obj }
+//    #TODO: Tools
+//    #TODO: Ingredient
+//    gon.recipe_ingredients = RecipeIngredient.order(:item_nb).where(recipe_id: gon.recipes.map{|r|r[:id]}).map {|e| e.to_obj}
+//    gon.notes = RecipeNote.where(recipe_id: gon.recipes.map{|r|r[:id]}).map {|e| e.to_obj}
+//    gon.foods = Food.order(:name).all.map {|food| food.to_obj}
+//    gon.units = Unit.all.map {|unit| unit.to_obj}
+//    gon.contractionList = FrenchExpression.where(contract_preposition: true).map(&:singular)
+//    #gon.user_recipes = current_user.recipes.order(:name).map {|r| r.to_obj(only: :name)}
+//    #gon.favorite_recipes = current_user.favorite_recipes.includes(:recipe).sort_by {|fav| fav.recipe.name}.map {|fav| o = fav.recipe.to_obj(only: :name); o[:fav_id] = fav.id; o}
+//  end
+//end
 
-function fetchUsers(req, res, next) {
-  db.all('SELECT * FROM users WHERE account_id = ?', [
-    req.user.id
-  ], function(err, rows) {
+function fetchTable(tableName, conditions, attributes, res, next) {
+  // TODO: Select only attributes instead of select *
+  // Then there is no need to extract
+  let s = 'SELECT '+['id',...attributes].join(', ')+' FROM '+tableName
+  let a = []
+  let l = Object.keys(conditions).length
+  if (l != 0) {s += ' WHERE '}
+  Object.keys(conditions).forEach((cond,i) => {
+    if (i < l-1) {s += ' AND '}
+    s += cond + ' = ?'
+    a.push(conditions[cond])
+  })
+  //console.log('fetchTable db statement: ', s)
+  db.all(s, a, function(err, rows) {
     if (err) { return next(err); }
-
-    var users = rows.map(function(row) {
-      return {
-        id: row.id,
-        email: row.email,
-        name: row.name
-      }
-    })
-    res.locals.users = users;
+    res.locals[tableName] = rows;
     next();
   })
+}
+
+
+function fetchUsers(req, res, next) {
+  fetchTable('users', {account_id: req.user.id}, ['email', 'name'], res, next)
 }
 
 function fetchRecipes(req, res, next) {
-  db.all('SELECT * FROM recipes WHERE user_id = ?', [
-    req.user.id
-  ], function(err, rows) {
-    if (err) { return next(err); }
-
-    var recipes = rows.map(function(row) {
-      return {
-        id: row.id,
-        name: row.name
-      }
-    })
-    res.locals.recipes = recipes;
-    next();
-  })
-
+  fetchTable('recipes', {user_id: req.user.id}, ['name', 'recipe_kind_id', 'main_ingredient_id', 'preparation_time', 'cooking_time', 'total_time', 'raw_servings', 'json', 'use_personalised_image', 'image_id', 'image_used_id'], res, next)
 }
 
 /* GET home page. */
