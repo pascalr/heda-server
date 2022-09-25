bc037e53fc0001852bf954f4a2358313f9c557b4
Dark Krael <dark.krael@gmail.com>
2022-09-25 09:20:27 -0400

    .

--- src/db.js
+++ src/db.js
@@ -1,5 +1,6 @@
 import sqlite3 from 'sqlite3';
 
+// FIXME: DB_URL should be DB_PATH, it's not an URL (doesn't start with sqlite3://...)
 var db = new sqlite3.Database(process.env.DB_URL);
 
 export default db;
