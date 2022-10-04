fab1dfd628af01201d658ff40d1961cd6e68686d
Dark Krael <dark.krael@gmail.com>
2022-10-04 09:22:41 -0400

    .

--- migrations/ingredients.js
+++ migrations/ingredients.js
@@ -5,28 +5,38 @@ import utils from '../src/utils.js';
 
 import {fetchTable, RECIPE_ATTRS} from '../src/gon.js';
 
-db.run('ALTER TABLE recipes ADD COLUMN ingredients TEXT', [], function(err) {
-  if (err) {console.log('ERR', err)}
-})
+// FIXME: I believe this is ran async, so everything should be nested inside for this to work...
+//db.run('ALTER TABLE recipes ADD COLUMN ingredients TEXT', [], function(err) {
+//  if (err) {console.log('ERR', err)}
+//})
 
 fetchTable('recipes', {}, RECIPE_ATTRS, () => {}, (recipes) => {
  
-  recipes.forEach(recipe => {
+  let attrs = ['before_ing_nb', 'name', 'recipe_id']
+  fetchTable('ingredient_sections', {}, attrs, () => {}, (ingredient_sections) => {
 
-    let attrs = ['item_nb', 'raw', 'comment_html', 'raw_food']
-    fetchTable('recipe_ingredients', {recipe_id: recipe.id}, attrs, () => {}, (records) => {
+    recipes.forEach(recipe => {
 
-      let text = ''
-      let ings = utils.sortBy(records, 'item_nb')
-      ings.forEach(ing => {
-        text += ing.raw + '; ' + ing.raw_food
-        if (ing.comment_html) {
-          text += ' ' + ing.comment_html
-        }
-        text += '\n'
-      })
-      db.run('UPDATE recipes SET ingredients = ? WHERE id = ?', [text, recipe.id], function(err) {
-        if (err) {console.log('ERR', err)}
+      let attrs = ['item_nb', 'raw', 'comment_html', 'raw_food']
+      fetchTable('recipe_ingredients', {recipe_id: recipe.id}, attrs, () => {}, (records) => {
+
+        let text = ''
+        let ings = utils.sortBy(records, 'item_nb')
+        ings.forEach(ing => {
+          let beforeNb = ing.item_nb == 1 ? null : ing.item_nb
+          let sections = ingredient_sections.filter(s => s.recipe_id == recipe.id && s.before_ing_nb == beforeNb)
+          sections.forEach(section => {
+            text += '# ' + section.name+ '\n'
+          })
+          text += ing.raw + '; ' + ing.raw_food
+          if (ing.comment_html) {
+            text += ' ' + ing.comment_html
+          }
+          text += '\n'
+        })
+        db.run('UPDATE recipes SET ingredients = ? WHERE id = ?', [text, recipe.id], function(err) {
+          if (err) {console.log('ERR', err)}
+        })
       })
     })
   })
