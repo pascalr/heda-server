adc15655dda2a6b93f2746df46e091524c4f63d6
Pascal Rainville <pascal.rainville@gmail.com>
2022-11-07 15:37:23 -0500

    .

---
+++ tests/db.js
@@ -0,0 +1,6 @@
+import { header, assertEquals } from "./tests_helpers.js"
+
+// FIXME: This with test database, not development database...
+//import db from '../src/db.js';
+
+let recipe = {name: 'Testing recipe'}
