facec19a0cc18e24be6db138863467742a3a8e97
Pascal Rainville <pascal.rainville@gmail.com>
2022-11-07 10:44:53 -0500

    .

--- src/lib.js
+++ src/lib.js
@@ -11,21 +11,14 @@ export function localeAttr(attr, locale) {
   return attr+'_'+(locale||'en')
 }
 export function fetchRecipeKinds(db, conditions, locale, fetchDescription=true) {
-  console.log('HERERERERERERE')
-  console.log('HERERERERERERE')
-  console.log('HERERERERERERE')
-  console.log('locale', locale)
   let attrs = [localeAttr('name', locale), 'image_slug']
   if (fetchDescription) {attrs.push(localeAttr('json', locale))}
   let records = db.fetchTable('recipe_kinds', conditions, attrs)
-  console.log('records', records)
-  let out = records.map(r => {
+  return records.map(r => {
     let obj = {id: r.id, image_slug: r.image_slug, name: r[localeAttr('name', locale)]}
     if (fetchDescription) {obj.description_json = r[localeAttr('json', locale)]}
     return obj
   })
-  console.log('out', out)
-  return out
 }
 export function fetchRecipeKind(db, conditions, locale, fetchDescription=true) {
   let attrs = [localeAttr('name', locale), 'image_slug']
