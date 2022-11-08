773dbf4762d2df80cfa28473484e98b47eee0dfe
Pascal Rainville <pascal.rainville@gmail.com>
2022-11-08 09:39:56 -0500

    .

--- src/db.js
+++ src/db.js
@@ -1,9 +1,12 @@
 import fs from 'fs';
 import path from 'path';
-import betterSqlite3 from 'better-sqlite3';
 
 import { ensureIsArray } from './utils.js';
 import schema from './schema.js';
+import sqlDb from './sql_db.js';
+
+//import { findRecipeKindForRecipeName } from "./lib.js"
+import utils from './utils.js';
 
 // FIXME: DB_URL should be DB_PATH, it's not an URL (doesn't start with sqlite3://...)
 let dbPath = process.env.DB_URL
@@ -14,11 +17,16 @@ if (fs.existsSync(dbPath)) {
 } else {
   console.log('no')
 }
-export const db = new betterSqlite3(dbPath, { verbose: console.log })
+export const db = new sqlDb(dbPath, { verbose: console.log })
+db.setSchema(schema)
 console.log('Opening database successful!')
 
-//import { findRecipeKindForRecipeName } from "./lib.js"
-import utils from './utils.js';
+//function injectFunctions(obj, functions) {
+//  functions.forEach(func => {
+//    if (obj[func]) {throw "Can't inject function it already exists."}
+//    obj[
+//  })
+//}
 
 // TODO: Use long version instead of short version:
 // DELETE FROM 'mixes' WHERE 'mixes.recipe_id' = 50.0
@@ -29,8 +37,7 @@ import utils from './utils.js';
 // Note: This does not work: DELETE FROM 'mixes' WHERE 'recipe_id' = 50.0
 
 const ALLOWED_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_'
-if (db.safe) {throw "Can't overide safe"}
-db.safe = function(str, allowed) {
+const safe = (str, allowed) => {
   let s = '';
   [...str].forEach(c => {
     if (ALLOWED_CHARS.includes(c)) {s += c}
@@ -108,7 +115,7 @@ db.safeUpdateRecord = function(table, old, updated, user, options={}) {
     if (old[f] != updated[f]) {fields.push(f)}
   })
   
-  let query = 'UPDATE '+db.safe(table, schemaHelper.getTableList())+' SET '+fields.map(field => db.safe(field, writeAttrs)+' = ?').join(', ')+', updated_at = ? WHERE id = ?'
+  let query = 'UPDATE '+safe(table, schemaHelper.getTableList())+' SET '+fields.map(field => safe(field, writeAttrs)+' = ?').join(', ')+', updated_at = ? WHERE id = ?'
 
   throw "TODO" // TODO
 }
@@ -120,7 +127,7 @@ db.safeUpdateField = function(table, id, field, value, user, options={}) {
 
   let writeAttrs = schemaHelper.getWriteAttributes(table)
   if (options.allow_write) {writeAttrs = [...writeAttrs, ...options.allow_write]}
-  let query0 = 'UPDATE '+db.safe(table, schemaHelper.getTableList())+' SET '+db.safe(field, writeAttrs)+' = ?, updated_at = ? WHERE id = ?'
+  let query0 = 'UPDATE '+safe(table, schemaHelper.getTableList())+' SET '+safe(field, writeAttrs)+' = ?, updated_at = ? WHERE id = ?'
   let args0 = [schemaHelper.validAttr(table, field, value), utils.now(), id]
   const [query, args] = addSafetyCondition(query0, args0, user, schemaHelper.getSecurityKey(table))
   return db.prepare(query).run(...args)
@@ -132,7 +139,7 @@ db.destroyRecord = function(table, id, conditions) {
   
   if (!table) {throw "Missing table for destroyRecord"}
 
-  const query0 = 'DELETE FROM '+db.safe(table, ALLOWED_TABLES_DESTROY)+' WHERE id = ?'
+  const query0 = 'DELETE FROM '+safe(table, ALLOWED_TABLES_DESTROY)+' WHERE id = ?'
   const args0 = [id]
   const [query, args] = appendConditions(query0, args0, conditions)
   let info = db.prepare(query).run(...args)
@@ -145,7 +152,7 @@ db.destroyRecordWithDependants = function(table, id, user) {
   if (!table) {throw "Missing table for destroyRecordWithDependants"}
 
   // First, make sure that the record is allowed to be destroyed by the user
-  let q = 'SELECT id FROM ' +db.safe(table, schemaHelper.getTableList())+' WHERE id = ?'
+  let q = 'SELECT id FROM ' +safe(table, schemaHelper.getTableList())+' WHERE id = ?'
   let [query, args] = addSafetyCondition(q, [id], user, schemaHelper.getSecurityKey(table))
   let r = db.prepare(query).get(...args)
   if (!r || !r.id) {throw "Error: Record not allowed to be destroyed."}
@@ -172,7 +179,7 @@ db.safeDestroyTableKey = function(table, key, val, user) {
   
   if (!table) {throw "Missing table for safeDestroyTableKey"}
 
-  const q = 'DELETE FROM '+db.safe(table, schemaHelper.getTableList())+' WHERE '+key+' = ?'
+  const q = 'DELETE FROM '+safe(table, schemaHelper.getTableList())+' WHERE '+key+' = ?'
   const [query, args] = addSafetyCondition(q, [val], user, schemaHelper.getSecurityKey(table))
   return db.prepare(query).run(...args)
 }
@@ -182,7 +189,7 @@ db.unsafeDestroyTableKey = function(table, key, val) {
   
   if (!table) {throw "Missing table for unsafeDestroyRecord"}
 
-  const q = 'DELETE FROM '+db.safe(table, schemaHelper.getTableList())+" WHERE "+key+" = ?"
+  const q = 'DELETE FROM '+safe(table, schemaHelper.getTableList())+" WHERE "+key+" = ?"
   return db.prepare(q).run(val)
 }
 
@@ -203,13 +210,13 @@ db.safeCreateRecord = function(table, unsafeObj, user, options={}) {
   let obj = schemaHelper.getAllowCreate(table)(user, unsafeObj)
   if (!obj) {throw "createRecord not allowed for current user"}
 
-  let safeTable = db.safe(table, schemaHelper.getTableList())
+  let safeTable = safe(table, schemaHelper.getTableList())
   obj = schemaHelper.beforeCreate(table, obj)
 
   let fields = Object.keys(obj)
   let columns = schemaHelper.getWriteAttributes(table) || []
   if (options.allow_write) {columns = [...columns, ...options.allow_write]}
-  let query = 'INSERT INTO '+safeTable+' (created_at,updated_at,'+fields.map(f => db.safe(f, columns)).join(',')+') '
+  let query = 'INSERT INTO '+safeTable+' (created_at,updated_at,'+fields.map(f => safe(f, columns)).join(',')+') '
   query += 'VALUES (?,?,'+fields.map(f=>'?').join(',')+')'
   let args = [utils.now(), utils.now(), ...fields.map(f => schemaHelper.validAttr(table, f, obj[f]))]
   
@@ -218,101 +225,5 @@ db.safeCreateRecord = function(table, unsafeObj, user, options={}) {
   return {...obj, id: info.lastInsertRowid}
 }
 
-if (db.createRecord) {throw "Can't overide createRecord"}
-db.createRecord = function(table, obj, options={}) {
-  
-  if (!table) {throw "Missing table for createRecord"}
-    
-  let safeTable = db.safe(table, schemaHelper.getTableList())
-  obj = schemaHelper.beforeCreate(table, obj)
-
-  let fields = Object.keys(obj)
-  let columns = schemaHelper.getWriteAttributes(table) || []
-  if (options.allow_write) {columns = [...columns, ...options.allow_write]}
-  let query = 'INSERT INTO '+safeTable+' (created_at,updated_at,'+fields.map(f => db.safe(f, columns)).join(',')+') '
-  query += 'VALUES (?,?,'+fields.map(f=>'?').join(',')+')'
-  let args = [utils.now(), utils.now(), ...fields.map(f => schemaHelper.validAttr(table, f, obj[f]))]
-  
-  let info = db.prepare(query).run(...args)
-  if (info.changes != 1) {throw "Error creating record from in "+table+"."}
-  return {...obj, id: info.lastInsertRowid}
-}
-
-if (db.doBackup) {throw "Can't overide doBackup"}
-db.doBackup = function() {
-
-  let outDir = process.env.DB_BACKUP_DIR
-  if (!outDir) {throw "Error backing up db: missing environment variable DB_BACKUP_DIR"}
-  
-  let date = new Date()
-  const months = ["janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","decembre"];
-  let month = months[date.getMonth()];
-  let filename = `${date.getFullYear()}_${month}_${date.getDate()}_${date.getHours()}_${date.getMinutes()}_${date.getSeconds()}.db`
-
-  let name = path.join(outDir, filename)
-  console.log('Starting backup up database to: ', name)
-  db.backup(name)
-  console.log('Database backup completed.')
-}
-
-const fetchStatement = (tableName, conditions, attributes, options) => {
-  
-  if (!tableName) {throw "Missing table for fetch"}
-
-  let s = 'SELECT '+['id',...attributes.map(a => '"'+a+'"')].join(', ')+' FROM '+db.safe(tableName, schemaHelper.getTableList())
-  let a = []
-  if (Array.isArray(conditions)) {
-    // The first element is the query, the second is the arguments
-    s += ' WHERE '+conditions[0]
-    a = ensureIsArray(conditions[1])
-  } else {
-    let l = Object.keys(conditions).length
-    if (l != 0) {s += ' WHERE '}
-    let keys = Object.keys(conditions)
-    for (let i = 0; i < keys.length; i++) {
-      //FIXME: Escape keys; let cond = "'"+keys[i]+"'"
-      //Maybe it's «'», maybe it's «"» that's required, I don't know and it makes a difference. Select 'name' was not working...
-      let cond = keys[i]
-      let val = conditions[cond]
-      if (val == null) {
-        s += cond + ' IS NULL'
-      } else if (Array.isArray(val) && val.length == 0) {
-        console.log('fetchStatement: info: an empty array given as a condition. Impossible match.')
-        return null
-      } else if (Array.isArray(val) && val.length > 1) {
-        s += cond + ' IN ('
-        val.forEach((v,i) => {
-          s += '?' + ((i < val.length - 1) ? ', ' : '')
-          a.push(v)
-        })
-        s += ')'
-      } else {
-        s += cond + ' = ?'
-        a.push(val)
-      }
-      if (i < l-1) {s += ' AND '}
-    }
-  }
-  if (options.limit) {s += ' LIMIT '+parseInt(options.limit, 10)}
-  console.log('statement:', s)
-  console.log('values', a)
-  return [s, a]
-}
-
-if (db.fetchTable) {throw "Can't overide fetchTable"}
-db.fetchTable = function(tableName, conditions, attributes, options={}) {
-  let o = fetchStatement(tableName, conditions, attributes, options)
-  if (!o) return []
-  let [s, a] = o
-  return db.prepare(s).all(...a)
-}
-
-if (db.fetchRecord) {throw "Can't overide fetchRecord"}
-db.fetchRecord = function(tableName, conditions, attributes, options={}) {
-  let o = fetchStatement(tableName, conditions, attributes, options)
-  if (!o) return null
-  let [s, a] = o
-  return db.prepare(s).get(...a)
-}
 
 export default db;
---
+++ src/sql_db.js
@@ -0,0 +1,135 @@
+import betterSqlite3 from 'better-sqlite3';
+import path from 'path';
+
+const getTableList = (schema) => {return Object.keys(schema)}
+const getWriteAttributes = (schema) => schema[table].write_attrs
+
+const validAttr = (schema, table, field, value) => {
+  let types = schema[table].attrs_types
+  if (types && types[field]) {
+    let type = types[field]
+    if (type == 'bool') {
+      return (value && value != 'false' && value != '0') ? 1 : 0
+    }
+  }
+  return value
+}
+
+const ALLOWED_CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_'
+const safe = (str, allowed) => {
+  let s = '';
+  [...str].forEach(c => {
+    if (ALLOWED_CHARS.includes(c)) {s += c}
+  })
+  if (str != s) {throw "Error: User tried to send unsafe value."}
+  if (!allowed.includes(s)) {throw "Error: Db value not allowed ("+s+")."}
+  return "'"+s+"'"
+}
+
+const fetchStatement = (schema, tableName, conditions, attributes, options) => {
+  
+  if (!tableName) {throw "Missing table for fetch"}
+
+  let s = 'SELECT '+['id',...attributes.map(a => '"'+a+'"')].join(', ')+' FROM '+safe(tableName, getTableList(schema))
+  let a = []
+  if (Array.isArray(conditions)) {
+    // The first element is the query, the second is the arguments
+    s += ' WHERE '+conditions[0]
+    a = ensureIsArray(conditions[1])
+  } else {
+    let l = Object.keys(conditions).length
+    if (l != 0) {s += ' WHERE '}
+    let keys = Object.keys(conditions)
+    for (let i = 0; i < keys.length; i++) {
+      //FIXME: Escape keys; let cond = "'"+keys[i]+"'"
+      //Maybe it's «'», maybe it's «"» that's required, I don't know and it makes a difference. Select 'name' was not working...
+      let cond = keys[i]
+      let val = conditions[cond]
+      if (val == null) {
+        s += cond + ' IS NULL'
+      } else if (Array.isArray(val) && val.length == 0) {
+        console.log('fetchStatement: info: an empty array given as a condition. Impossible match.')
+        return null
+      } else if (Array.isArray(val) && val.length > 1) {
+        s += cond + ' IN ('
+        val.forEach((v,i) => {
+          s += '?' + ((i < val.length - 1) ? ', ' : '')
+          a.push(v)
+        })
+        s += ')'
+      } else {
+        s += cond + ' = ?'
+        a.push(val)
+      }
+      if (i < l-1) {s += ' AND '}
+    }
+  }
+  if (options.limit) {s += ' LIMIT '+parseInt(options.limit, 10)}
+  console.log('statement:', s)
+  console.log('values', a)
+  return [s, a]
+}
+
+//if (db.fetchTable) {throw "Can't overide fetchTable"}
+const sqlDb = {
+  setSchema(schema) {
+    this.___schema = schema
+  },
+  getSchema() {
+    return this.___schema
+  },
+  fetchTable(tableName, conditions, attributes, options={}) {
+    let o = fetchStatement(this.getSchema(), tableName, conditions, attributes, options)
+    if (!o) return []
+    let [s, a] = o
+    return this.prepare(s).all(...a)
+  },
+  fetchRecord(tableName, conditions, attributes, options={}) {
+    let o = fetchStatement(this.getSchema(), tableName, conditions, attributes, options)
+    if (!o) return null
+    let [s, a] = o
+    return db.prepare(s).get(...a)
+  },
+  createRecord(table, obj, options={}) {
+    
+    if (!table) {throw "Missing table for createRecord"}
+    
+    let schema = this.getSchema()
+    let safeTable = safe(table, getTableList(schema))
+  
+    let fields = Object.keys(obj)
+    let columns = getWriteAttributes(schema) || []
+    if (options.allow_write) {columns = [...columns, ...options.allow_write]}
+    let query = 'INSERT INTO '+safeTable+' (created_at,updated_at,'+fields.map(f => safe(f, columns)).join(',')+') '
+    query += 'VALUES (?,?,'+fields.map(f=>'?').join(',')+')'
+    let args = [utils.now(), utils.now(), ...fields.map(f => validAttr(schema, table, f, obj[f]))]
+    
+    let info = this.prepare(query).run(...args)
+    if (info.changes != 1) {throw "Error creating record from in "+table+"."}
+    return {...obj, id: info.lastInsertRowid}
+  },
+  doBackup() {
+  
+    let outDir = process.env.DB_BACKUP_DIR
+    if (!outDir) {throw "Error backing up db: missing environment variable DB_BACKUP_DIR"}
+    
+    let date = new Date()
+    const months = ["janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","decembre"];
+    let month = months[date.getMonth()];
+    let filename = `${date.getFullYear()}_${month}_${date.getDate()}_${date.getHours()}_${date.getMinutes()}_${date.getSeconds()}.db`
+  
+    let name = path.join(outDir, filename)
+    console.log('Starting backup up database to: ', name)
+    this.backup(name)
+    console.log('Database backup completed.')
+  },
+}
+function injectFunctions(obj, functionsObj) {
+  Object.keys(functionsObj).map(name => {
+    if (obj.prototype[name]) {throw "Can't inject function becasue it already exists."}
+    obj.prototype[name] = functionsObj[name]
+  })
+}
+injectFunctions(betterSqlite3, sqlDb)
+
+export default betterSqlite3
