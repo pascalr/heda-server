ebd5e4497fb9ee2682c8ded475a5e70a5f211118
Pascal Rainville <pascal.rainville@gmail.com>
2022-11-08 12:41:48 -0500

    .

--- src/schema.js
+++ src/schema.js
@@ -8,42 +8,21 @@
 const schema = {
   'translations': {
     write_attrs: ['from', 'original', 'to', 'translated'],
-    security_key: 'ADMIN_ONLY',
-    allow_create(user, obj) {
-      if (!user.is_admin) {return null}
-      return obj
-    },
-    allow_create_2: (user, obj) => user.is_admin,
     is_allowed: user => user.is_admin,
   },
   'images': {
     write_attrs: ['filename', 'author', 'is_user_author', 'source'],
     attrs_types: {is_user_author: 'bool'},
-    security_key: 'user_id',
-    allow_create(user, obj) {
-      obj.user_id = user.user_id
-      return obj
-    },
     security_attrs: ['user_id'],
     // When creating an object, set the security attribute based on the security object
     // When updating, fetch the object to make sure the security attribute is the same as the one given from the security object
   },
   'recipe_kinds': {
     write_attrs: ['image_slug', 'name_fr', 'json_fr', 'name_en', 'json_en'],
-    security_key: 'ADMIN_ONLY',
-    allow_create(user, obj) {
-      if (!user.is_admin) {return null}
-      return obj
-    },
     is_allowed: user => user.is_admin,
   },
   'translated_recipes': {
     write_attrs: ['name', 'servings_name', 'ingredients', 'json'],
-    security_key: 'original_id',
-    allow_create(user, obj) {
-      obj.original_id = user.original_id
-      return obj
-    },
     security_attrs: ['original_id'],
   },
   'recipes': {
@@ -51,53 +30,23 @@ const schema = {
     security_key: 'user_id',
     dependant_destroy: {recipe_id: ['favorite_recipes', 'meals', 'mixes', 'recipe_comments', 'recipe_notes', 'recipe_ratings', 'recipe_tools', 'references', 'suggestions']},
 
-    before_create(recipe) {
-      //const recipeKinds = db.fetchTable('recipe_kinds', {}, ['name'])
-      //const recipeKind = findRecipeKindForRecipeName(recipe.name, recipeKinds)
-      //if (recipeKind) {recipe.recipe_kind_id = recipeKind.id}
-      return recipe
-    },
-    allow_create(user, obj) {
-      obj.user_id = user.user_id
-      return obj
-    },
     security_attrs: ['user_id'],
   },
   'users': {
     write_attrs: ['name', 'gender', 'image_slug', 'locale', 'is_public'],
     attrs_types: {is_public: 'bool'},
-    security_key: 'account_id',
-    allow_create(user, obj) {
-      obj.account_id = user.account_id
-      return obj
-    },
     security_attrs: ['account_id'],
   },
   'favorite_recipes': {
     write_attrs: ['list_id', 'recipe_id'],
-    security_key: 'user_id',
-    allow_create(user, obj) {
-      obj.user_id = user.user_id
-      return obj
-    },
     security_attrs: ['user_id'],
   },
   'tags': {
     write_attrs: ['name', 'image_slug', 'position'],
-    security_key: 'user_id',
-    allow_create(user, obj) {
-      obj.user_id = user.user_id
-      return obj
-    },
     security_attrs: ['user_id'],
   },
   'suggestions': {
     write_attrs: ['tag_id', 'recipe_id'],
-    security_key: 'user_id',
-    allow_create(user, obj) {
-      obj.user_id = user.user_id
-      return obj
-    },
     security_attrs: ['user_id'],
   },
   'meals': {
