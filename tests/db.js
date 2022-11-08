3896d6a7c6e4838a5143190fdca894185aeebd1d
Pascal Rainville <pascal.rainville@gmail.com>
2022-11-08 10:21:58 -0500

    .

--- tests/db.js
+++ tests/db.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 
-import { header, assertEquals } from "./tests_helpers.js"
+import { header, assertEquals, assertThrowsException, fail, pass } from "./tests_helpers.js"
 import sqlDb from "../src/sql_db.js"
 import schema from '../src/schema.js';
 
@@ -17,7 +17,6 @@ let user = {user_id: 999999999}
 header('Testing createRecord')
 let recipe = db.createRecord('recipes', {name: 'Testing recipe'}, user)
 assertEquals(user.user_id, recipe.user_id)
-try {
+assertThrowsException('Trying to mass assign a security attribute should throw an exception', () => {
   db.createRecord('recipes', {user_id: 1}, user)
-  fail('Trying to mass assign a security attribute should throw an exception')
-} catch (e) {}
+})
--- tests/tests_helpers.js
+++ tests/tests_helpers.js
@@ -6,6 +6,10 @@ export function fail(msg) {
   console.log('\x1b[31mFAILED\x1b[0m', msg);
 }
 
+export function pass(msg) {
+  console.log('\x1b[32mPASSED\x1b[0m', msg);
+}
+
 export function header(msg) {
   console.log('\x1b[0;94m%s\x1b[0m', msg);
 }
@@ -15,6 +19,12 @@ export function assertEquals(expected, actual) {
   console.log(coloredResult(expected === actual))
 }
 
+export function assertThrowsException(msg, func) {
+  try {
+    func()
+    fail(msg)
+  } catch (e) {pass(msg)}
+}
 
 export function assertStartsWith(expected, actual) {
   console.log('Expected starts with:', expected, 'Input:', actual)
