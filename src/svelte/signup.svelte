fec0e683ef039bcebe2d1035518c430a9f95c022
Pascal Rainville <pascal.rainville@gmail.com>
2022-12-06 09:06:25 -0500

    .

--- src/svelte/signup.svelte
+++ src/svelte/signup.svelte
@@ -6,15 +6,15 @@
       <div class="inform-error">{error}</div><br/>
     {/each}
   {/if}
-  <form action={urlWithLocale('/signup', locale)} method="post">
+  <form action={urlWithLocale('/signup', locale)} method="post" on:submit={submitForm}>
     <div>
       <label for="username">{t('Username')} (public)</label>
-      <input id="username" name="username" class="form-control" type="text" autocomplete="email" required>
+      <input id="username" name="username" class="form-control" type="text" autocomplete="email">
     </div>
     <br/>
     <div>
       <label for="email">{t('Email')}</label>
-      <input id="email" name="email" class="form-control" type="text" autocomplete="email" on:blur={validateEmail} required>
+      <input id="email" name="email" class="form-control" type="text" autocomplete="email" on:blur="{validateEmail}" bind:value={email} bind:this={emailInput}>
     </div>
     <br/>
     {#if emailError}
@@ -22,7 +22,7 @@
     {/if}
     <div>
       <label for="new-password">{t('Password')}</label>
-      <input id="new-password" name="password" class="form-control" type="password" autocomplete="new-password" on:blur={validatePassword} required>
+      <input id="new-password" name="password" class="form-control" type="password" autocomplete="new-password" on:blur={validatePassword} bind:value={password} bind:this={passwordInput}>
     </div>
     <br/>
     {#if passwordError}
@@ -47,27 +47,37 @@
   let csrf = getCsrf()
   let errors = window.errors
   let t = translate(locale)
+  let email = '', password = ''
+  let emailInput, passwordInput
 
   let emailError = ''
   let passwordError = ''
 
   function validateEmail() {
-    if (isValidEmail(this.value)) {
-      this.classList.remove('invalid')
+    if (isValidEmail(email)) {
+      emailInput.classList.remove('invalid')
       emailError = ''
     } else {
-      this.classList.add('invalid')
+      emailInput.classList.add('invalid')
       emailError = 'Please enter a valid email address.'
     }
   }
 
   function validatePassword() {
-    if (isValidPassword(this.value)) {
-      this.classList.remove('invalid')
+    if (isValidPassword(password)) {
+      passwordInput.classList.remove('invalid')
       passwordError = ''
     } else {
-      this.classList.add('invalid')
+      passwordInput.classList.add('invalid')
       passwordError = 'Password must be at least 6 characters long.'
+    }
+  }
+
+  function submitForm(e) {
+    console.log('this', this)
+    if (!isValidEmail() || !isValidPassword()) {
+      e.preventDefault()
+      return false
     }
   }
 </script>
