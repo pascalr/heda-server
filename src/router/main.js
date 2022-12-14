3598bc777741d7a1a084c379fc25eb9eb19a71a0
Pascal Rainville <pascal.rainville@gmail.com>
2022-12-14 09:59:46 -0500

    .

--- src/router/main.js
+++ src/router/main.js
@@ -306,11 +306,6 @@ const renderApp = [ensureUser, function(req, res, next) {
 
   let user = req.user
   let o = {}
-  let attrs = null
-  let ids = null
-  //o.users = db.fetchTable('users', {account_id: user.account_id}, ['name', 'image_slug', 'locale', 'is_public'])
-  // o.users = db.fetchTable('users', {account_id: user.account_id}, ['name', 'image_slug', 'locale'])
-  // let profile = o.users.find(u => u.id == user.user_id)
   o.siblings = db.fetchTable('users', {account_id: user.account_id}, ['name']).filter(u => u.id != user.id)
   o.recipes = db.fetchTable('recipes', {user_id: user.user_id}, RECIPE_ATTRS)
   o.favorite_recipes = db.fetchTable('favorite_recipes', {user_id: user.user_id}, ['list_id', 'recipe_id', 'updated_at'])
@@ -328,38 +323,12 @@ const renderApp = [ensureUser, function(req, res, next) {
   //ids = o.machines.map(m=>m.id)
   //attrs = ['jar_id', 'machine_id', 'container_format_id', 'pos_x', 'pos_y', 'pos_z', 'food_id']
   //o.containers = db.fetchTable('containers', {machine_id: ids}, attrs)
-  //attrs = ['user_id', 'recipe_id', 'tag_id', 'score']
-  //o.suggestions = db.fetchTable('suggestions', {user_id: user.user_id}, attrs)
-  //attrs = ['name', 'image_slug', 'user_id', 'position']
-  //o.tags = db.fetchTable('tags', {user_id: user.user_id}, attrs)
   //o.foods = db.fetchTable('foods', {}, ['name'])
   //attrs = ['name', 'value', 'is_weight', 'is_volume', 'show_fraction']
   //o.units = db.fetchTable('units', {}, attrs)
-
-  //let suggestionTagsIds = [16,9,17]
-  //o.suggestion_tags = db.fetchTable('tags', {id: suggestionTagsIds}, ['name'])
-  //o.recipe_suggestions = {}
-  //o.suggestion_tags.forEach(t => {
-  //  let results = db.prepare("SELECT recipes.id, recipes.name, recipes.image_slug FROM suggestions JOIN recipes ON suggestions.recipe_id = recipes.id JOIN users ON recipes.user_id = users.id WHERE users.is_public = 1 AND users.id <> ? AND recipes.image_slug IS NOT NULL AND recipes.image_slug <> '' AND suggestions.tag_id = ? ORDER BY random() LIMIT 10;").all(user.user_id, t.id)
-  //  if (results && results.length > 0) {o.recipe_suggestions[t.name] = results}
-  //})
-
-  //o.recipe_suggestions = db.prepare("SELECT recipes.id, recipes.name, recipes.image_slug, users.name AS user_name FROM recipes JOIN users ON recipes.user_id = users.id WHERE users.is_public = 1 AND users.id <> ? AND recipes.image_slug IS NOT NULL AND recipes.image_slug <> '' ORDER BY random() LIMIT 10;").all(user.user_id)
-
-  //let slugs1 = o.recipes.map(r=>r.image_slug).filter(x=>x)
-  ////let slugs2 = o.recipe_kinds.map(r=>r.image_slug).filter(x=>x)
-  //let slugs3 = o.tags.map(t=>t.image_slug).filter(x=>x)
-  //ids = [...slugs1, ...slugs3].map(s => s.split('.')[0])
-  //attrs = ['author', 'source', 'filename', 'is_user_author', 'slug']
-  //o.images = db.fetchTable('images', {id: ids}, attrs)
-
   //o.machine_foods = db.fetchTable('machine_foods', {}, ['food_id', 'machine_id'])
 
-
-  // FIXME: FRIENDS OR "SAME ACCOUNT" RECIPES
-  // ids = o.users.map(u => u.id).filter(id => id != user.user_id)
-  // o.friends_recipes = db.fetchTable('recipes', {user_id: ids}, ['name', 'user_id', 'image_slug', 'recipe_kind_id'])
-  o.friends_recipes = db.fetchTable('recipes', {user_id: req.user.id}, ['name', 'user_id', 'image_slug', 'recipe_kind_id'])
+  o.friends_recipes = db.fetchTable('recipes', {user_id: o.siblings.map(u => u.id)}, ['name', 'user_id', 'image_slug', 'recipe_kind_id'])
 
   res.locals.gon = {...res.locals.gon, ...o}
   next()
